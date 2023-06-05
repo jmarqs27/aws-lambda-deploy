7c9ca9d094d6e04489d2729ef0c697e25d97d701
Junior Marques <junior.uniseb8@gmail.com>
2023-06-05 14:49:49 -0300

    Update 3

--- index.mjs
+++ index.mjs
@@ -1,27 +1,23 @@
-import { log } from "./log.mjs";
-import { S3 } from "@aws-sdk/client-s3";
+import { log } from './log.mjs';
+import { S3 } from '@aws-sdk/client-s3';
 
-const s3Client = new S3({ region : 'us-east-2' })
-
-export const handler = async(event, _, callback) => {
-
+const s3Client = new S3({ region: 'us-east-2' });
+export const handler = async(event) => {
     const record = event.Records[0];
-    const Bucket = record.S3.bucket.name;
-    const key = record.S3.object.key;
-    const getObjectResult = s3Client.getObject({
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
         Bucket,
-        key,
-    }).then(getObjectResult => {
-        const mega_byte = 1024 * 1024;
-    
-        if (getObjectResult.ContentLength > 1 * mega_byte) {
-            log ( ' É um objeto grande. ');
-            
-            callback('Big Object');
-        }
-        else {
-            log (' Objeto de tamanho normal. ')
-            callback('Ok');
-        }
+        Key,
     });
+    const mega_byte = 1024 * 1024;
+
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto muito grande');
+
+        return 'Objeto muito grande';
+    }
+
+    log('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
 };
