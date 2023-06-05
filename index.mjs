1adb64691949c61588a7dc13072b21bbdf9a2194
Junior Marques <junior.uniseb8@gmail.com>
2023-06-05 14:18:07 -0300

    Buscando Objetos do S3

--- index.mjs
+++ index.mjs
@@ -1,14 +1,27 @@
 import { log } from "./log.mjs";
+import { s3 } from "@aws-sdk/client-s3";
 
-export const handler = async(event) => {
+const s3Client = new s3({ region : 'us-east-2' })
 
-    log('Adicionando Log de execução após github action com Váriavel de Ambiente  . event: ' + JSON.stringify(event));
+export const handler = async(event, _, callback) => {
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(event),
-        Headers: {
-            'Content-Type': 'application/json'
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const key = record.s3.object.key;
+    const getObjectResult = s3Client.getObject({
+        Bucket,
+        key,
+    }).then(getObjectResult => {
+        const mega_byte = 1024 * 1024;
+    
+        if (getObjectResult.ContentLength > 1 * mega_byte) {
+            log ( ' É um objeto grande. ');
+            
+            callback('Big Object');
         }
-    };
+        else {
+            log (' Objeto de tamanho normal. ')
+            callback('Ok');
+        }
+    });
 };
